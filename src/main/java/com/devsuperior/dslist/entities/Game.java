598340ccf84f7b7a1ab093028a5f77685f5519ca
ari-junior <ari.magagnin@gmail.com>
2023-05-08 22:06:13 -0300

    Game Seed

--- src/main/java/com/devsuperior/dslist/entities/Game.java
+++ src/main/java/com/devsuperior/dslist/entities/Game.java
@@ -26,10 +26,10 @@ public class Game {
 	private Double score;
 	private String imgUrl;
 	
-	//@Column(columnDefinition = "TEXT")
+	@Column(columnDefinition = "TEXT")
 	private String shortDescription;
 
-	//@Column(columnDefinition = "TEXT")
+	@Column(columnDefinition = "TEXT")
 	private String longDescription;
 	
 	public Game() {
